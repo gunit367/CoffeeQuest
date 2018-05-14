efed458e5eb7df37d2e27988852130c2eb6cf86e
Grant Blake <gunit367@gmail.com>
2018-05-13 18:27:17 -0700

    Initial Commit

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPosition : MonoBehaviour
+{
+    const int UP = 0;
+    const int RIGHT = 1;
+    const int DOWN = 2;
+    const int LEFT = 3;
+
+    public int x;
+    public int y;
+    public int[] walls; // UP, RIGHT, DOWN, LEFT
+    public GridPosition[] neighbors;
+    public int group;
+
+    public GridPosition(int x, int y, int group)
+    {
+        this.x = x;
+        this.y = y;
+        this.walls = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            this.walls[i] = 1;
+        }
+
+        this.neighbors = new GridPosition[4];
+        for (int i = 0; i < 4; i++)
+        {
+            this.neighbors[i] = null;
+        }
+        this.group = group;
+    }
+
+    int Opposite(int dir)
+    {
+        switch (dir)
+        {
+            case UP:
+                return DOWN;
+            case DOWN:
+                return UP;
+            case RIGHT:
+                return LEFT;
+            case LEFT:
+                return RIGHT;
+        }
+        return 0;
+    }
+
+    int Min(int x, int y)
+    {
+        if (x < y)
+        {
+            return x;
+        }
+        return y;
+    }
+
+    public void RemoveWall(int dir)
+    {
+        this.walls[dir] = 0;
+        GridPosition n = this.neighbors[dir];
+        n.walls[Opposite(dir)] = 0;
+
+        int g = Min(this.group, n.group);
+        n.SetGroup(g);
+        this.SetGroup(g);
+    }
+
+    public int SeparateNeighbor()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (this.neighbors[i] != null)
+            {
+                if (this.neighbors[i].group != this.group)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public void SetGroup(int n)
+    {
+        if (this.group == n)
+        {
+            return;
+        }
+        this.group = n;
+        for (int i = 0; i < 4; i++)
+        {
+            if (this.walls[i] == 0)
+            {
+                this.neighbors[i].SetGroup(n);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Maze : MonoBehaviour
+{
+    int GRID_WIDTH;
+    int GRID_HEIGHT;
+    int TRIES_MAX;
+
+    const int UP = 0;
+    const int RIGHT = 1;
+    const int DOWN = 2;
+    const int LEFT = 3;
+
+    GridPosition[,] grid;
+    private int separate_components;
+    private int tmp;
+
+    public Maze()
+    {
+        this.GRID_WIDTH = 3;
+        this.GRID_HEIGHT = 3;
+        this.TRIES_MAX = 2;
+        InitializeMaze();
+    }
+
+    public void InitializeMaze()
+    {
+        this.grid = new GridPosition[GRID_WIDTH, GRID_HEIGHT];
+        for (int i = 0; i < GRID_WIDTH; i++)
+        {
+            for (int j = 0; j < GRID_HEIGHT; j++)
+            {
+                int a0;
+                int a1;
+                int b;
+
+                if (GRID_WIDTH > GRID_HEIGHT)
+                {
+                    b = GRID_WIDTH;
+                    a0 = i;
+                    a1 = j;
+                }
+                else
+                {
+                    b = GRID_HEIGHT;
+                    a0 = j;
+                    a1 = i;
+                }
+
+                int group = b * a0 + a1;
+
+                this.grid[i, j] = new GridPosition(i, j, group);
+            }
+        }
+        this.CreateGraph();
+        this.separate_components = GRID_WIDTH * GRID_HEIGHT;
+        this.tmp = 1;
+    }
+
+    public void CreateMaze()
+    {
+        while (this.separate_components > 1)
+        {
+            this.JoinRandomComponents();
+        }
+    }
+
+    private void CreateGraphRecursive(int x, int y)
+    {
+        GridPosition pos = this.grid[x, y];
+        GridPosition left = null;
+        GridPosition up = null;
+
+        if (pos.neighbors[LEFT] != null || pos.neighbors[UP] != null)
+        {
+            return; // Already visited
+        }
+
+        if (x > 0)
+        {
+            left = this.grid[x - 1, y];
+            pos.neighbors[LEFT] = left;
+            left.neighbors[RIGHT] = pos;
+            CreateGraphRecursive(x - 1, y);
+        }
+
+        if (y > 0)
+        {
+            up = this.grid[x, y - 1];
+            pos.neighbors[UP] = up;
+            up.neighbors[DOWN] = pos;
+            CreateGraphRecursive(x, y - 1);
+        }
+    }
+
+    private void CreateGraph()
+    {
+        CreateGraphRecursive(GRID_WIDTH - 1, GRID_HEIGHT - 1);
+    }
+
+    private Pair GetRandomPosition()
+    {
+        int x = 0;
+        int y = 0;
+        int t;
+        Pair res;
+
+        if (this.tmp == 1)
+        {
+            int tries = 0;
+            res = new Pair(Random.Range(0, GRID_WIDTH), Random.Range(0, GRID_HEIGHT));
+            while (grid[res.x, res.y].SeparateNeighbor() == -1)
+            {
+                res = new Pair(Random.Range(0, GRID_WIDTH), Random.Range(0, GRID_HEIGHT));
+                tries += 1;
+
+                if (tries == TRIES_MAX)
+                {
+                    this.tmp = 0;
+                    return res;
+                }
+            }
+            return res;
+        }
+
+        t = this.grid[x, y].SeparateNeighbor();
+        while (t == -1)
+        {
+            x += 1;
+            if (x == GRID_WIDTH)
+            {
+                x = 0;
+                y += 1;
+            }
+            t = this.grid[x, y].SeparateNeighbor();
+        }
+        return new Pair(x, y);
+    }
+
+    private void JoinRandomComponents()
+    {
+        Pair coords = GetRandomPosition();
+        GridPosition p = this.grid[coords.x, coords.y];
+        for (int i = 3; i >= 0; i--)
+        {
+            if (p.neighbors[i] != null)
+            {
+                if (p.neighbors[i].group != p.group)
+                {
+                    p.RemoveWall(i);
+                    this.separate_components -= 1;
+                    return;
+                }
+            }
+        }
+        this.tmp = 0;
+    }
+
+    public void NextMaze()
+    {
+        GRID_HEIGHT += 1;
+        GRID_WIDTH += 1;
+        this.InitializeMaze();
+    }
+
+
+    private class Pair
+    {
+        public int x;
+        public int y;
+
+        public Pair(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
